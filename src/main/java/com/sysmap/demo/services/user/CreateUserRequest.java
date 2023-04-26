5937d5a3786752ea3e0761aca33b1c285d9b9043
Le1ec0 <leandroizidoro@hotmail.com>
2023-04-26 18:37:34 -0300

    Corrected password property

--- src/main/java/com/sysmap/demo/services/user/CreateUserRequest.java
+++ src/main/java/com/sysmap/demo/services/user/CreateUserRequest.java
@@ -3,5 +3,5 @@ package com.sysmap.demo.services.user;
 public class CreateUserRequest {
     public String name;
     public String email;
-    public String Password;
+    public String password;
 }
