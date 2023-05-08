4abdcd1a393cdaa052087070b261abb5c506bb59
Le1ec0 <leandroizidoro@hotmail.com>
2023-05-08 18:46:14 -0300

    Implemented UserController

--- src/main/java/com/sysmap/demo/api/UserController.java
+++ src/main/java/com/sysmap/demo/api/UserController.java
@@ -1,6 +1,5 @@
 package com.sysmap.demo.api;
 
-import com.sysmap.demo.services.security.IJwtService;
 import com.sysmap.demo.services.user.CreateUserRequest;
 import com.sysmap.demo.services.user.FindUserResponse;
 import com.sysmap.demo.services.user.IUserService;
@@ -8,23 +7,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.UUID;
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
     @Autowired
     private IUserService _userService;
-    @Autowired
-    private IJwtService _jwtService;
-    @PostMapping()
+    @PostMapping("create")
     public ResponseEntity<String> createUser(@RequestBody CreateUserRequest request) {
-        if (_jwtService.isValidToken(getToken(), getUserId())) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("User not authenticated");
-        }
-
         var response = _userService.createUser(request);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
@@ -35,12 +26,15 @@ public class UserController {
         return ResponseEntity.ok().body(_userService.findUserByEmail(email));
     }
 
-    public String getToken() {
-        var jwt =  (((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest().getHeader("Authorization"));
-        return jwt.substring(7);
-    }
+    @PostMapping("/photo/upload")
+    public ResponseEntity uploadPhotoProfile(@RequestParam("photo") MultipartFile photo) {
+        try {
+            _userService.uploadPhotoProfile(photo);
 
-    public String getUserId() {
-        return (((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest().getHeader("UserId"));
+            return new ResponseEntity(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+
     }
 }
