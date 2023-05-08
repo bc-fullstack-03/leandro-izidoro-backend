3f6784f5e616ac1d56b807bd66e101c295491f0c
Le1ec0 <leandroizidoro@hotmail.com>
2023-05-08 18:45:49 -0300

    Implemented JwtService

--- src/main/java/com/sysmap/demo/services/security/JwtService.java
+++ src/main/java/com/sysmap/demo/services/security/JwtService.java
@@ -1,5 +1,6 @@
 package com.sysmap.demo.services.security;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
@@ -9,38 +10,51 @@ import org.springframework.stereotype.Service;
 import java.security.Key;
 import java.util.Date;
 import java.util.UUID;
+import java.util.function.Function;
 
 @Service
 public class JwtService implements IJwtService {
 
     private final long EXPIRATION_TIME = 7200000;
-    private final String KEY = "u7x!A%D*F-JaNdRgUkXp2s5v8y/B?E(H";
+    private final String KEY = "404E635266546A576E5A7234753778214125442A472D4B6150645367566B5870";
 
-    public String generateToken(String email) {
+    public String generateToken(UUID userId) {
         return Jwts
                 .builder()
-                .setSubject(userId)
+                .setSubject(userId.toString())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .signWith(genSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
 
-    public boolean IsValidToken (String token, UUID userId) {
-        var claims = Jwts.parserBuilder().setSigningKey(genSignInKey()).build().parseClaimsJws((token).getbody());
+    public boolean isValidToken(String token, String userId) {
+        var sub = getClaim(token, Claims::getSubject);
+        var tExpiration = getClaim(token, Claims::getExpiration);
 
-        var sub = claims.getSubject();
-        var tExpiration = claims.getExpiration();
+        if (!sub.equals(userId)) {
+            return false;
+        }
 
-        return userId.toString().equals(sub) && tExpiration.before(new Date());
+        if (tExpiration.before(new Date())) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private <T> T getClaim(String token, Function<Claims, T> claimsResolver) {
+        var claims = Jwts
+                .parserBuilder()
+                .setSigningKey(genSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claimsResolver.apply(claims);
     }
 
     private Key genSignInKey() {
         return Keys.hmacShaKeyFor(Decoders.BASE64.decode(KEY));
-    }
-
-    @Override
-    public String generateToken(UUID userId) {
-        return null;
     }
 }
