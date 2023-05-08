1c665369a8e3831df71ae1faf953f4466698d638
Le1ec0 <leandroizidoro@hotmail.com>
2023-05-08 18:45:15 -0300

    Implemented IAuthenticationService

--- src/main/java/com/sysmap/demo/services/authentication/IAuthenticationService.java
+++ src/main/java/com/sysmap/demo/services/authentication/IAuthenticationService.java
@@ -1,4 +1,5 @@
 package com.sysmap.demo.services.authentication;
 
 public interface IAuthenticationService {
+    AuthenticateResponse authenticate(AuthenticateRequest request) throws Exception;
 }
