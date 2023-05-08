b658a2b6afb005272459b3c883be04569ed10900
Le1ec0 <leandroizidoro@hotmail.com>
2023-05-08 18:45:39 -0300

    Implemented IUserService

--- src/main/java/com/sysmap/demo/services/user/IUserService.java
+++ src/main/java/com/sysmap/demo/services/user/IUserService.java
@@ -1,6 +1,14 @@
 package com.sysmap.demo.services.user;
 
+import com.sysmap.demo.entities.User;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
+
 public interface IUserService {
     String createUser(CreateUserRequest request);
     FindUserResponse findUserByEmail(String email);
+    User getUser(String email);
+    User getUserById(UUID id);
+    void uploadPhotoProfile(MultipartFile photo) throws Exception;
 }
