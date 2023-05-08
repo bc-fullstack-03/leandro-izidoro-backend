8cfa121a8b82b4888a36fa56268027ca3f216c13
Le1ec0 <leandroizidoro@hotmail.com>
2023-05-08 18:40:56 -0300

    Implemented AuthenticationService

--- src/main/java/com/sysmap/demo/services/authentication/AuthenticationService.java
+++ src/main/java/com/sysmap/demo/services/authentication/AuthenticationService.java
@@ -3,24 +3,28 @@ package com.sysmap.demo.services.authentication;
 import com.sysmap.demo.services.security.IJwtService;
 import com.sysmap.demo.services.user.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationService implements IAuthenticationService {
+
     @Autowired
     private IUserService _userService;
     @Autowired
     private IJwtService _jwtService;
+    @Autowired
+    private PasswordEncoder _passwordEncoder;
 
-    public AuthenticateResponse authenticate(AuthenticateRequest request) {
+    public AuthenticateResponse authenticate(AuthenticateRequest request) throws Exception {
         var user = _userService.getUser(request.email);
 
-        if(user == null){
+        if (user == null) {
             return null;
         }
 
-        if (!user.getPassword().equals(request.password)) {
-            return null;
+        if (!_passwordEncoder.matches(request.password, user.getPassword())) {
+            throw new Exception("Credenciais inválidas!");
         }
 
         var token = _jwtService.generateToken(user.getId());
