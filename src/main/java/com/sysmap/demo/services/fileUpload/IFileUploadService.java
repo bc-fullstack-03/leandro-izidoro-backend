a1b01687187d8036ba99a9e1947092cc6044c0e7
Le1ec0 <leandroizidoro@hotmail.com>
2023-05-08 18:45:23 -0300

    Implemented IFileUploadService

--- src/main/java/com/sysmap/demo/services/fileUpload/IFileUploadService.java
+++ src/main/java/com/sysmap/demo/services/fileUpload/IFileUploadService.java
@@ -1,4 +1,7 @@
 package com.sysmap.demo.services.fileUpload;
 
+import org.springframework.web.multipart.MultipartFile;
+
 public interface IFileUploadService {
+    String upload(MultipartFile file, String fileName);
 }
