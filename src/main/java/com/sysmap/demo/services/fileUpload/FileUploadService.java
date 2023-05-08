4d8b67dde2c41515e5772f0bcd73fbfff5be77d6
Le1ec0 <leandroizidoro@hotmail.com>
2023-05-08 18:45:00 -0300

    Implemented FileUploadService

--- src/main/java/com/sysmap/demo/services/fileUpload/FileUploadService.java
+++ src/main/java/com/sysmap/demo/services/fileUpload/FileUploadService.java
@@ -1,4 +1,24 @@
 package com.sysmap.demo.services.fileUpload;
 
-public class FileUploadService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class FileUploadService implements IFileUploadService {
+
+    @Autowired
+    private AwsService _awsService;
+
+    public String upload(MultipartFile file, String fileName) {
+        var fileUri = "";
+
+        try {
+            fileUri = _awsService.upload(file, fileName);
+        } catch (Exception e) {
+
+        }
+
+        return fileUri;
+    }
 }
