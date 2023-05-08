e535a1a3d4f9839d87edc6f845e8312e70cc325d
Le1ec0 <leandroizidoro@hotmail.com>
2023-05-08 18:45:29 -0300

    Implemented IJwtService

--- src/main/java/com/sysmap/demo/services/security/IJwtService.java
+++ src/main/java/com/sysmap/demo/services/security/IJwtService.java
@@ -2,7 +2,7 @@ package com.sysmap.demo.services.security;
 
 import java.util.UUID;
 
-public interface IJwtService extends JwtService {
+public interface IJwtService {
     String generateToken(UUID userId);
-
+    boolean isValidToken(String token, String userId);
 }
