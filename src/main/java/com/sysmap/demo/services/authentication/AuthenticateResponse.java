01a0f8bf3c4897e0ae3736fc7e7ff8e5cdccf210
Le1ec0 <leandroizidoro@hotmail.com>
2023-05-08 18:40:22 -0300

    Implemented AuthenticateResponse

--- src/main/java/com/sysmap/demo/services/authentication/AuthenticateResponse.java
+++ src/main/java/com/sysmap/demo/services/authentication/AuthenticateResponse.java
@@ -1,4 +1,11 @@
 package com.sysmap.demo.services.authentication;
 
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
 public class AuthenticateResponse {
+    public UUID userId;
+    public String token;
 }
