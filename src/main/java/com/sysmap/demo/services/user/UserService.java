90c7d43075d103b1a8590fa25318782e44f7094b
Le1ec0 <leandroizidoro@hotmail.com>
2023-05-08 18:46:38 -0300

    Implemented UserService

--- src/main/java/com/sysmap/demo/services/user/UserService.java
+++ src/main/java/com/sysmap/demo/services/user/UserService.java
@@ -2,10 +2,14 @@ package com.sysmap.demo.services.user;
 
 import com.sysmap.demo.data.UserRepository;
 import com.sysmap.demo.entities.User;
-import com.sysmap.demo.services.IEventService;
+import com.sysmap.demo.services.fileUpload.IFileUploadService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Async;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
 
 @Service
 public class UserService implements IUserService {
@@ -13,11 +17,21 @@ public class UserService implements IUserService {
     @Autowired
     private UserRepository _userRepository;
     @Autowired
-    private IEventService _eventService;
+    private PasswordEncoder _passwordEncoder;
+    @Autowired
+    private IFileUploadService _fileUploadService;
 
-    @Async
+
     public String createUser(CreateUserRequest request) {
-        var user = new User(request.name, request.email, request.password);
+        var user = new User(request.name, request.email);
+
+        if (!_userRepository.findUserByEmail(request.email).isEmpty()) {
+            return null;
+        }
+
+        var hash = _passwordEncoder.encode(request.password);
+
+        user.setPassword(hash);
 
         _userRepository.save(user);
 
@@ -27,8 +41,33 @@ public class UserService implements IUserService {
     public FindUserResponse findUserByEmail(String email) {
         var user = _userRepository.findUserByEmail(email).get();
 
-        var response = new FindUserResponse(user.getId(), user.getName(), user.getEmail());
+        var response = new FindUserResponse(user.getId(), user.getName(), user.getEmail(), user.getPhotoUri());
 
         return response;
+    }
+
+    public User getUser(String email) {
+        return _userRepository.findUserByEmail(email).get();
+    }
+
+    public User getUserById(UUID id) {
+        return _userRepository.findUserById(id).get();
+    }
+
+    public void uploadPhotoProfile(MultipartFile photo) throws Exception {
+        var user = ((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+
+        var photoUri = "";
+
+        try {
+            var fileName = user.getId() + "." + photo.getOriginalFilename().substring(photo.getOriginalFilename().lastIndexOf(".") + 1);
+
+            photoUri = _fileUploadService.upload(photo, fileName);
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+
+        user.setPhotoUri(photoUri);
+        _userRepository.save(user);
     }
 }
