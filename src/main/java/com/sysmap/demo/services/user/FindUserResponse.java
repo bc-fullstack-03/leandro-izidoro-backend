738b6100aee7ecd6ba1689b95e261e005a96e8d2
Le1ec0 <leandroizidoro@hotmail.com>
2023-05-08 18:45:07 -0300

    Implemented FindUserResponse

--- src/main/java/com/sysmap/demo/services/user/FindUserResponse.java
+++ src/main/java/com/sysmap/demo/services/user/FindUserResponse.java
@@ -6,10 +6,12 @@ public class FindUserResponse {
     public UUID id;
     public String name;
     public String email;
+    public String photoUri;
 
-    public FindUserResponse(UUID id, String name, String email) {
+    public FindUserResponse(UUID id, String name, String email, String photoUri) {
         this.id = id;
         this.name = name;
         this.email = email;
+        this.photoUri = photoUri;
     }
 }
